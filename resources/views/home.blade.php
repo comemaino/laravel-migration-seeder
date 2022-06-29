a79ac8572bba2e01a09b76bb728e713cb0f40c2d
comemaino <mat.maino@gmail.com>
2022-06-29 16:19:27 +0200

    Add: departure_date today filter

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,19 @@
 <div class="container">
 	<ul>
-		@foreach ($trains as $train)
+		@foreach ($trains->where('departure_date', '2022-06-29') as $train)
 			<li>
 				<span>Train #{{ $train->railway_sys_id }} | </span>
 				<span>Departing from: {{ $train->departure_station }} </span>
 				<span>on {{ $train->departure_date }} </span>
 				<span>at {{ $train->departure_time }} | </span>
-				<span>Arriving at {{ $train->arrival_station }} | </span>
-				<span>Delayed:
-					@if ($train->on_time)
-						<span>NO</span>
-					@else
-						<span>YES</span>
-					@endif
-					|
-				</span>
-				<span>Cancelled:
-					@if ($train->cancelled)
-						<span>YES</span>
-					@else
-						<span>NO</span>
-					@endif
-				</span>
+				<span>Arriving at {{ $train->arrival_station }} </span>
+				<span>at {{ $train->arrival_time }} | </span>
+				@if (!$train->on_time && !$train->cancelled)
+					<span>DELAYED</span>
+				@endif
+				@if ($train->cancelled)
+					<span>CANCELLED</span>
+				@endif
 			</li>
 		@endforeach
 	</ul>
