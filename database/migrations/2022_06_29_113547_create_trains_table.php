34de431af11c512f3c3e46b9d4fc075bb8fe12d7
comemaino <mat.maino@gmail.com>
2022-06-29 13:58:48 +0200

    Add: trains table migration + update migration

---
+++ database/migrations/2022_06_29_113547_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('maker', 48);
+            $table->string('departure_station', 48);
+            $table->string('arrival_station', 48);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->unsignedSmallInteger('#');
+            $table->unsignedTinyInteger('carriages');
+            $table->boolean('on_time');
+            $table->boolean('cancelled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
